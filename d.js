9defd727c92ab5cb77d9f566554800f6a5b41696
lesha7 <122871102+lesha7@users.noreply.github.com>
2023-01-30 14:22:09 +0300

    Add files via upload

--- d.js
+++ d.js
@@ -312,14 +312,30 @@
  //     } 
  //     n223.textContent = a 
  //   }
-//  24
-//  let elem1 = document.querySelector("#elem1")
-// let elem2 = document.querySelector("#elem2")
-// let elem3 = document.querySelector("#elem3")
-// let p = document.querySelector("#p")
-// let button = document.querySelector("#button")
-// button.addEventListener("click",func)
-// function func(){
-//  p.textContent = String(Math.sqrt(elem1.value)) +" "+ String(Math.sqrt(elem2.value))+" " + String(Math.sqrt(elem3.value))
 
-// } 
+ //  24
+//  let inpA = document.querySelector("#inpA")
+//  let inpB = document.querySelector("#inpB")
+//  let inpC = document.querySelector("#inpC")
+//  let p = document.querySelector("#p")
+//  let button = document.querySelector("#button")
+//  button.addEventListener("click",func)
+//  function func(){
+//      let a = inpA.value
+//      let b = inpB.value
+//      let c = inpC.value
+//      let x = b*b-4*a*c
+//      if (x<0){
+//          x1 = "нет корней"
+//          x2 = "нет корней"
+//      }
+//      else if (x===0){
+//          x1=-b/(2*a)
+//          x2=x1
+//      }
+//      else {
+//          x1=(-b+Math.sqrt(x))/(2*a)
+//          x2=(-b+Math.sqrt(x))/(2*a)
+//      }
+//      p.textContent = x1+" "+x2
+//  }
